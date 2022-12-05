981a8887296a6ab84ad06f107f154347bb08d888
BakareHameed <bakarehameed07@gmail.com>
2022-12-05 21:19:40 +0100

    rules compenent

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import close from './images/icon-close.svg'
+import rules from './images/image-rules.svg'
+import './App.css'
 
 const Rules = () => {
   return (
-    <div>Rules</div>
+    // implemented the rules page
+    <div className='flex bg-white h-[350px] w-[350px] mx-auto rounded-md p-5'>
+      <div className='flex flex-col justify-center gap-5'>
+        <div className='flex justify-between'>
+          <h1 className='dark-text'>RULES</h1>
+          <img src={close} alt="close"/>
+        </div>
+        <img src={rules} alt="rules"/>
+      </div>
+    </div>
   )
 }
 
